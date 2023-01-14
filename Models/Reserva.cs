455a42f1869bf4a69b30d6689c645a9deb7ac4f8
Gustavo <gustavobatistamelo@gmail.com>
2023-01-14 11:48:14 -0300

    Desafio Concluidogit status!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -40,16 +40,14 @@ namespace DesafioProjetoHospedagem.Models
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // Implementado
+            decimal valor =  DiasReservados * Suite.ValorDiaria ;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado
+            if ( 10 <= DiasReservados )
             {
-                valor = 0;
+                valor *=  0.9M;
             }
 
             return valor;
